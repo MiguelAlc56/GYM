59c3437f48bc65adca83f35149aee4e391f83392
MiguelAlc56 <isc_malcala2021@accitesz.com>
2025-05-14 04:06:59 -0600

    Servicio Empleados act

---
+++ schema/EmpleadosSchema.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from .PersonaSchema import Base, Persona
+
+class Empleado(Base):
+    __tablename__ = 'empleados'
+
+    idEmpleados = Column(Integer, primary_key=True, autoincrement=True)
+    Contraseña=Column(String(45),nullable=False)
+    Roll=Column(String(45),nullable=False, Default="Recepcionista")
+    Estatus = Column(String(45), nullable=False)
+    id_persona = Column(Integer, ForeignKey('persona.idPersona'), nullable=False)
+
+    persona = relationship("Persona")
